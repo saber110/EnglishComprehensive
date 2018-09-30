5a8138dfa932b42a6d4b428bbaa26d008da155b5
saber <15675123342@163.com>
2018-09-30 21:44:16 +0800

    all category

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ import urllib
 from bs4 import BeautifulSoup
 from Spider import Spider
 from wordsDeal import wordsDeal
-def going():
+def going(url):
     text = wordsDeal()
-    spider1 = Spider('https://www.csmonitor.com/World/(view)/all', 'test')
+    spider1 = Spider(url, 'test')
     hrefs = spider1.hrefFor2018()
     for item in hrefs:
         content = str(spider1.contentOfArtical(item['href']))
@@ -15,11 +15,15 @@ def going():
             query = "insert into Christian(href,title, content) values ( '" + text.sqlEscape(item['href']) + "','" + text.sqlEscape(item['title']) + "','" + text.sqlEscape(content) + "');"
             sqlQuery(query)
 	    print item['title'], "|" , wordNum
-
+    del spider1
 
 def sqlQuery(sql = ""):
     db = mysql.connect()
     mysql.query(db,sql)
     mysql.close(db)
 
-going()
+def main():
+    for item in config.category:
+        going(item)
+
+main()
---
+++ test.py
@@ -0,0 +1,4 @@
+import config
+
+for item in config.category:
+    print item, "|"
