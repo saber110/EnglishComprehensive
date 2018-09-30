ac9fa3ebdf91e3a9217a9f1a9c3791b24343c024
saber <15675123342@163.com>
2018-09-30 16:01:40 +0800

    count number of words

--- run.py
+++ run.py
@@ -3,15 +3,18 @@ import mysql, config
 import urllib
 from bs4 import BeautifulSoup
 from Spider import Spider
-
+from wordsDeal import wordsDeal
 def going():
+    text = wordsDeal()
     spider1 = Spider('https://www.csmonitor.com/World/(view)/all', 'test')
     hrefs = spider1.hrefFor2018()
     for item in hrefs:
         content = str(spider1.contentOfArtical(item['href']))
-        query = "insert into Christian(href,title, content) values ( '" + sqlEscape(item['href']) + "','" + sqlEscape(item['title']) + "','" + sqlEscape(content) + "');"
-        sqlQuery(query)
-	print item['title']
+        wordNum = text.getNumber(content)
+        if wordNum > config.minimum and wordNum < config.maximum:
+            query = "insert into Christian(href,title, content) values ( '" + text.sqlEscape(item['href']) + "','" + text.sqlEscape(item['title']) + "','" + text.sqlEscape(content) + "');"
+            sqlQuery(query)
+	    print item['title'], "|" , wordNum
 
 
 def sqlQuery(sql = ""):
@@ -19,10 +22,4 @@ def sqlQuery(sql = ""):
     mysql.query(db,sql)
     mysql.close(db)
 
-def sqlEscape(old = ""):
-    temp = old.replace("'", "\\'")
-    new  =temp.replace('"', '\\"')
-    return new
-
-# sqlQuery("select * from Christian");
 going()
---
+++ wordsDeal.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class wordsDeal:
+    '字处理模块'
+    def __init__(self):
+        pass
+
+    def sqlEscape(self, old=""):
+        '特殊字符转义'
+        temp = old.replace("'", "\\'")
+        new  =temp.replace('"', '\\"')
+        return new
+
+    def getNumber(self, str=""):
+        strList = str.split(' ')
+        length1 = len(strList)
+        length2 = strList.count('')
+        return length1 - length2
+
+# a = wordsDeal()
+# a.getNumber("JM is a big pig, so what is your ")
