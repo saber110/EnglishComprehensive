a27d59ff4e398be7c492c25fa5d8e54a29785d5a
root <root@vultr.guest>
2018-09-30 07:36:15 +0000

    pass

--- run.py
+++ run.py
@@ -9,14 +9,20 @@ def going():
     hrefs = spider1.hrefFor2018()
     for item in hrefs:
         content = str(spider1.contentOfArtical(item['href']))
-        query = "insert into Christian(href,title, content) values ( '" + item['href'] + "','" + item['title'] + "','" + content + ");"
+        query = "insert into Christian(href,title, content) values ( '" + sqlEscape(item['href']) + "','" + sqlEscape(item['title']) + "','" + sqlEscape(content) + "');"
         sqlQuery(query)
+	print item['title']
 
 
 def sqlQuery(sql = ""):
     db = mysql.connect()
     mysql.query(db,sql)
     mysql.close(db)
+
+def sqlEscape(old = ""):
+    temp = old.replace("'", "\\'")
+    new  =temp.replace('"', '\\"')
+    return new
 
 # sqlQuery("select * from Christian");
 going()
